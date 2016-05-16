aae51ff7f69b1f64bf64ab5ad06afdfe169b3faa
Rowan Cota <cota.rg@gmail.com>
2016-05-16 15:45:13 -0700

    wrote some validations for my user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  # validations
+  validates :provider, presence: true # must be string: spotify
+  validates :uid, presence: true # must be string
+  validates :name, # if present must be string
+
 end
