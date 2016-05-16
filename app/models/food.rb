093f955cd0290cf553444560643e2af7a3499950
Rowan Cota <cota.rg@gmail.com>
2016-05-16 16:43:05 -0700

    scaffolded in the pieces that will come in the JSON data for reference.

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,10 @@
 class Food < ActiveRecord::Base
+
+  # business_id string  Yelp-specific ID, which can be used to make calls to the Yelp Business API to retrieve complete details
+  # name  string  Name of the business
+  # url string  Yelp URL for this business
+  # image_url string  URL of the photo to display for this business
+  # phone string  Phone number for the business
+  # rating  decimal number  Average rating for this business based on Yelp reviews
+
 end
--- app/models/music.rb
+++ app/models/music.rb
@@ -1,2 +1,9 @@
 class Music < ActiveRecord::Base
+  # item_id string  Spotify-specific ID, which can be used in conjunction with the type to make calls to the Spotify API
+  # type  string  Which type of resource this item is. Can be: artist, album, track, playlist
+  # name  string  Name of the item
+  # url string  URL for opening this item in a browser-based Spotify player
+  # image_url string  URL of the photo to display for this item
+
 end
+
