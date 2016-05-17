feade49733ba38a1516c8b28546797fb0b1f2096
Rowan Cota <cota.rg@gmail.com>
2016-05-17 15:07:52 -0700

    working on my music model.

--- app/models/music.rb
+++ app/models/music.rb
@@ -1,4 +1,6 @@
 class Music < ActiveRecord::Base
+  BASE_MUSIC_URI = "https://api.spotify.com"
+
   # item_id string  Spotify-specific ID, which can be used in conjunction with the type to make calls to the Spotify API
   # type  string  Which type of resource this item is. Can be: artist, album, track, playlist
   # name  string  Name of the item
