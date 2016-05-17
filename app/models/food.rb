5749302a2b9eacbd62bcfe3fafc3652ae8acbfbd
Rowan Cota <cota.rg@gmail.com>
2016-05-17 11:13:12 -0700

    wrote a self#find and an initializer.

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,25 @@
+require 'httparty'
+
 class Food < ActiveRecord::Base
+  BASE_FOOD_URI = "https://api.yelp.com/v2/business/"
+  attr_reader :name, :phone, :rating
+
+  # Using BFU, concatenating business_id should deliver info about the chosen restaurant
+
+  def initialize(data)
+    @business_id = data["business_id"]
+    @name = data["name"]
+    @url = data["url"]
+    @image_url = data["image_url"]
+    @phone = data["phone"]
+    @rating = data["rating"]
+  end
+
+  def self.find(business_id)
+    data = HTTParty.get(BASE_FOOD_URI + "#{business_id}").parsed_response
+
+    self.new(data)
+  end
 
   # business_id string  Yelp-specific ID, which can be used to make calls to the Yelp Business API to retrieve complete details
   # name  string  Name of the business
