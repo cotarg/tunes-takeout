77b9f2f2a1b2c4bfc1da94c37325b65cf5e2129e
Rowan Cota <cota.rg@gmail.com>
2016-05-17 15:34:41 -0700

    setting up a users table.

--- db/migrate/20160516221205_create_users.rb
+++ db/migrate/20160516221205_create_users.rb
@@ -1,6 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string :provider, null: false
+      t.string :uid, null: false
+      t.string :name, null: false
+      t.string :image
 
       t.timestamps null: false
     end
