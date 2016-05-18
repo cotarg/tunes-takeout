b1222c0f25e16f13e60190aace5d3c4613ae6aff
Rowan Cota <cota.rg@gmail.com>
2016-05-17 22:17:42 -0700

    working on creating an authed user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,31 @@ class User < ActiveRecord::Base
   validates :uid, presence: true # must be string
   validates :name, # if present must be string
 
+  def self.find_or_create_by_omniauth(auth_hash)
+    # Find or create a user
+    user = self.find_by(provider: auth_hash["provider"], uid: auth_hash["uid"])
+    
+    if !user.nil?
+      return user
+    else
+      self.make_user(auth_hash)
+    end
+  end
+
+  private
+
+  def self.make_user
+    user = User.new
+    user.name = auth_hash["info"]["name"]
+    user.uid = auth_hash["uid"]
+    user.email = auth_hash["info"]["email"]
+    user.provider = auth_hash["provider"]
+    user.image = auth_hash["info"]["image"]
+    if user.save
+      return user
+    else
+      return nil
+    end
+  end
+
 end
