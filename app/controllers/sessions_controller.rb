e384544dc3713dbd9d91d7626bcb22bff7cff3d6
Rowan Cota <cota.rg@gmail.com>
2016-05-18 09:24:36 -0700

    started working on a sessions controller so folks can log in.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
   def create
     auth_hash = request.env['omniauth.auth']
     user = User.find_or_create_by_omniauth(auth_hash)
+    raise
 
     if user
       session[:user_id] = user.id
