d76c6a5a0b46edd1d5bbbd773bd99b406d6e2f14
Rowan Cota <cota.rg@gmail.com>
2016-05-17 22:17:58 -0700

    wrote an index method to render a sample view.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
 
   def index
+    # eventually we'll need to call the API to generate some popular pairs here.
     render :index
   end
+
+  
 
 end
