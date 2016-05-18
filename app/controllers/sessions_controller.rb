c2499b6036c6707deadb9d1ffbfe8ca5c1663eda
Rowan Cota <cota.rg@gmail.com>
2016-05-17 22:56:13 -0700

    integrating sessions and users for login.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
 
   def create
     auth_hash = request.env['omniauth.auth']
-    user = User.find_or_create_from_omniauth(auth_hash)
+    user = User.find_or_create_by_omniauth(auth_hash)
 
     if user
       session[:user_id] = user.id
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,32 +1,24 @@
 class User < ActiveRecord::Base
-  # validations
   validates :name, :uid, :provider, presence: true
 
   def self.find_or_create_by_omniauth(auth_hash)
     # Find or create a user
-    user = self.find_by(provider: auth_hash["provider"], uid: auth_hash["uid"])
+    user = self.find_by(provider: auth_hash["provider"], uid: auth_hash["id"])
     
     if !user.nil?
       return user
     else
-      make_user(auth_hash)
+      user = User.new
+      user.uid = auth_hash["id"]
+      user.provider = auth_hash["provider"]
+      user.name = auth_hash["info"]["name"]
+      user.email = auth_hash["info"]["email"]
+      user.image = auth_hash["info"]["image"]
+      if user.save
+        return user
+      else
+        return nil
+      end
     end
   end
-
-  private
-
-  def make_user
-    user = User.new
-    user.name = auth_hash["info"]["name"]
-    user.uid = auth_hash["uid"]
-    user.email = auth_hash["info"]["email"]
-    user.provider = auth_hash["provider"]
-    user.image = auth_hash["info"]["image"]
-    if user.save
-      return user
-    else
-      return nil
-    end
-  end
-
 end
