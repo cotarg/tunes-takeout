2e5be93695ea6709fd603067dc60cc4a5ed72b62
Rowan Cota <cota.rg@gmail.com>
2016-05-19 09:55:56 -0700

    getting the spotify_user variable working.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,19 +9,19 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create
-    @user = User.new(user_create_params[:user])
-    if @user.save
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
+  # def create
+  #   @user = User.new(user_create_params[:user])
+  #   if @user.save
+  #     redirect_to root_path
+  #   else
+  #     render :new
+  #   end
+  # end
 
-  private
+  # private
 
-  def user_create_params
-    params.permit(user: [:name, :email, :password, :password_confirmation])
-  end
+  # def user_create_params
+  #   params.permit(user: [:name, :email, :password, :password_confirmation])
+  # end
 
 end
