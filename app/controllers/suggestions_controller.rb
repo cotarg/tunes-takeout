3e838d863d53960ad1159763f32b659907b0fc53
Rowan Cota <cota.rg@gmail.com>
2016-05-19 15:54:59 -0700

    working wrapper, working top method. need to develop interaction with spotify&&yelp.

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,13 +1,12 @@
 require 'HTTParty'
+require 'TakeoutTunesWrapper'
+
 
 class SuggestionsController < ApplicationController
-  BASE_URL = "https://tunes-takeout-api.herokuapp.com/"
-
 
   def index
     @user = User.find_by(uid: session[:user_id])
-    @suggestions = HTTParty.get(BASE_URL + "/v1/suggestions/top").parsed_response
-    raise
+    @suggestions = TakeoutTunesWrapper.top(20)
 
     render :index
   end
@@ -15,7 +14,7 @@ class SuggestionsController < ApplicationController
   def show
     # this method needs to use the TakeoutTunes wrapper to get some suggestion pairs.
     # then it needs to deliver them to the suggestion partial, one at a time.
-    @suggestions = TunesTakeoutWrapper.search()
+    @suggestions = TakeoutTunesWrapper.search()
   end
 
 end
--- lib/TakeoutTunesWrapper.rb
+++ lib/TakeoutTunesWrapper.rb
@@ -1,12 +1,14 @@
-module TunesTakeoutWrapper
+require 'HTTParty'
+
+module TakeoutTunesWrapper
   BASE_URL = "https://tunes-takeout-api.herokuapp.com/"
 
     def self.search(keyword)
       @data = HTTParty.get(BASE_URL + "v1/suggestions/search?query=" + keyword).parsed_response
     end
 
-    def self.top(number_of_suggestions)
-      @data = HTTParty.get(BASE_URL + "/v1/suggestions/top").parsed_response
+    def self.top(number_of_suggestions=20)
+      @data = HTTParty.get(BASE_URL + "/v1/suggestions/top?limit=#{number_of_suggestions}").parsed_response
     end
 
 end
