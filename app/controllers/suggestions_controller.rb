7124f64f0d8778168083e48518b7cd37876a0016
Rowan Cota <cota.rg@gmail.com>
2016-05-19 14:18:44 -0700

    working on generating suggestions.

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -4,5 +4,11 @@ class SuggestionsController < ApplicationController
     @user = User.find_by(uid: session[:user_id])
     render :index
   end
-  
+
+  def find
+    # this method needs to use the TakeoutTunes wrapper to get some suggestion pairs.
+    # then it needs to deliver them to the suggestion partial, one at a time.
+    @suggestions = TunesTakeoutWrapper.search()
+  end
+
 end
