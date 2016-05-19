6b9225cb65809fe868ea13d417a500f9d6274253
Rowan Cota <cota.rg@gmail.com>
2016-05-19 15:21:24 -0700

    wrote a wrapper method for CharlesAPI but it's not working yet.

--- lib/TakeoutTunesWrapper.rb
+++ lib/TakeoutTunesWrapper.rb
@@ -1,11 +1,12 @@
 module TunesTakeoutWrapper
   BASE_URL = "https://tunes-takeout-api.herokuapp.com/"
-  # def initialize(data)
-  # end
 
-  def self.search(keyword)
-    @data = HTTParty.get(BASE_URL + "v1/suggestions/search?query=" + keyword).parsed_response
-  end
+    def self.search(keyword)
+      @data = HTTParty.get(BASE_URL + "v1/suggestions/search?query=" + keyword).parsed_response
+    end
 
-  
+    def self.top(number_of_suggestions)
+      @data = HTTParty.get(BASE_URL + "/v1/suggestions/top").parsed_response
+    end
+
 end
