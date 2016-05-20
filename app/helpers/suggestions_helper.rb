f655eb84403b62aa5559d0b1f7f77ef20f8b9d79
Rowan Cota <cota.rg@gmail.com>
2016-05-20 10:49:54 -0700

    helper methods to compose suggestion object
    writing methods in view helper to assemble an object that can be passed to the partial for rendering that contains all information for a suggestion in one place.

--- app/helpers/suggestions_helper.rb
+++ app/helpers/suggestions_helper.rb
@@ -1,2 +1,30 @@
 module SuggestionsHelper
+
+  def convert_suggestion_id_to_food_asset(result_of_suggestion_pair)
+    # takes a suggestion.new
+    # runs Yelp.client.business search on Food ID
+
+    yelp_hash = Yelp.client.business(result_of_suggestion_pair["food_id"]).parsed_response
+
+    # returns object containing Yelp.client.business search for digestion by partial
+  end
+
+  def convert_suggestion_id_to_spotofy_asset(result_of_suggestion_pair)
+    # takes a suggestion.new
+    # runs Spotify search on music ID, music type
+    spotify_hash = 
+
+    # returns object with Spotify search
+  end
+
+  def construct_single_suggestion_complete_object(pair_id)
+    # converts a pair_id to a pair object
+    pair = Suggestion.new(pair_id)
+    yelp = convert_suggestion_id_to_food_asset(pair)
+    spotify = 
+
+  end
+
+
+
 end
