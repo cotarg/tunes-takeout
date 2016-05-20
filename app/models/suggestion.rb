5cd3864d116d20917fd3a15f496cb277d2addc82
Rowan Cota <cota.rg@gmail.com>
2016-05-20 10:50:46 -0700

    removed whitespace, changed a variable.

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -3,12 +3,6 @@ require 'RSpotify'
 require 'yelp'
 require 'TakeoutTunesWrapper'
 
-client = Yelp::Client.new({ consumer_key: ENV["YELP_CONSUMER_KEY"],
-                            consumer_secret: ENV["YELP_CONSUMER_SECRET"],
-                            token: ENV["YELP_TOKEN"],
-                            token_secret: ENV["YELP_TOKEN_SECRET"]
-                          })
-
 class Suggestion < ActiveRecord::Base
 
   def initialize(pair_id)
@@ -19,7 +13,7 @@ class Suggestion < ActiveRecord::Base
   end
 
   def self.yelp_hash(pair_id)
-    business_hash = client.business(TakeoutTunesWrapper.food_id(pair_id))
+    business_hash = Yelp.client.business(TakeoutTunesWrapper.food_id(pair_id))
   end
 
 end
