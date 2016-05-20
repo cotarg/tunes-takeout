b5cd5e4184f2fb9e775974391e053e314e736e88
Rowan Cota <cota.rg@gmail.com>
2016-05-20 08:59:43 -0700

    working on getting assets from spotify/yelp.

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,23 +1,18 @@
 require 'HTTParty'
 require 'TakeoutTunesWrapper'
-require 'yelp'
 
-client = Yelp::Client.new({ consumer_key: ENV["YELP_CONSUMER_KEY"],
-                            consumer_secret: ENV["YELP_CONSUMER_SECRET"],
-                            token: ENV["YELP_TOKEN"],
-                            token_secret: ENV["YELP_TOKEN_SECRET"]
-                          })
+
 
 
 class SuggestionsController < ApplicationController
+
 
   def index
     @user = User.find_by(uid: session[:user_id])
     @suggestions = TakeoutTunesWrapper.top(20)["suggestions"]
 
     render :index
-    raise
-    
+
   end
 
 
