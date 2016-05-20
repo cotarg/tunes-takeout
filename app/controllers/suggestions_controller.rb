a2f29b40807c54e3ecb36867b74aadeb2e9e3148
Rowan Cota <cota.rg@gmail.com>
2016-05-19 20:57:52 -0700

    working towards method for getting yelp ids.

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,5 +1,12 @@
 require 'HTTParty'
 require 'TakeoutTunesWrapper'
+require 'yelp'
+
+client = Yelp::Client.new({ consumer_key: ENV["YELP_CONSUMER_KEY"],
+                            consumer_secret: ENV["YELP_CONSUMER_SECRET"],
+                            token: ENV["YELP_TOKEN"],
+                            token_secret: ENV["YELP_TOKEN_SECRET"]
+                          })
 
 
 class SuggestionsController < ApplicationController
@@ -10,11 +17,18 @@ class SuggestionsController < ApplicationController
 
     render :index
   end
+  
+  def yelp_hash(pair_id)
+    business_hash = client.business(TakeoutTunesWrapper.food_id(pair_id))
+  end
+
 
   def show
     # this method needs to use the TakeoutTunes wrapper to get some suggestion pairs.
     # then it needs to deliver them to the suggestion partial, one at a time.
     @suggestions = TakeoutTunesWrapper.search()
   end
+
+  private
 
 end
