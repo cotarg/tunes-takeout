5031038e952a01e6a4b689d90b33f024917e39a5
Rowan Cota <cota.rg@gmail.com>
2016-05-19 19:49:06 -0700

    generating all the data I need for pairs to display the top 20 suggestions.

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -1,2 +1,21 @@
+require 'HTTParty'
+require 'RSpotify'
+require 'yelp'
+require 'TakeoutTunesWrapper'
+
+client = Yelp::Client.new({ consumer_key: ENV["YELP_CONSUMER_KEY"],
+                            consumer_secret: ENV["YELP_CONSUMER_SECRET"],
+                            token: ENV["YELP_TOKEN"],
+                            token_secret: ENV["YELP_TOKEN_SECRET"]
+                          })
+
 class Suggestion < ActiveRecord::Base
+
+  def initialize(pair_id)
+    @pair = pair_id
+    @food_id = TakeoutTunesWrapper.food_id(pair_id)
+    @music_id = TakeoutTunesWrapper.music_id(pair_id)
+    @music_type = TakeoutTunesWrapper.music_type(pair_id)
+  end
+
 end
--- lib/TakeoutTunesWrapper.rb
+++ lib/TakeoutTunesWrapper.rb
@@ -6,11 +6,27 @@ module TakeoutTunesWrapper
   BASE_URL = "https://tunes-takeout-api.herokuapp.com/"
 
     def self.search(keyword)
-      @data = HTTParty.get(BASE_URL + "v1/suggestions/search?query=" + keyword).parsed_response
+      @suggestion_search = HTTParty.get(BASE_URL + "v1/suggestions/search?query=" + keyword).parsed_response
     end
 
     def self.top(number_of_suggestions=20)
-      @data = HTTParty.get(BASE_URL + "/v1/suggestions/top?limit=#{number_of_suggestions}").parsed_response
+      @top_suggestions = HTTParty.get(BASE_URL + "v1/suggestions/top?limit=#{number_of_suggestions}").parsed_response
+    end
+
+    def self.pair(pair_id)
+      @pair_hash = HTTParty.get(BASE_URL + "v1/suggestions/#{pair_id}").parsed_response
+    end
+
+    def self.music_id(pair_id)
+      @music_id = self.pair(pair_id)["music_id"]
+    end
+
+    def self.music_type(pair_id)
+      self.pair(pair_id)["music_type"]
+    end
+
+    def self.food_id(pair_id)
+      self.pair(pair_id)["food_id"]
     end
 
 end
